0796a33f73f93e788f5bfc4d106d2b6c5f0b898e
Ben Mizera <mizerb@rpi.edu>
2017-09-19 18:42:16 -0400

    Added command parsing, kinda. The start of know which command is input is there

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "time"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
 
 const ( //iota is reset to 0
 	INSERT = iota // INSERT=0
@@ -13,6 +19,36 @@ type tweet struct {
 	clock   time.Time
 }
 
+type block struct {
+	targetuser int
+	user       int
+	clock      time.Time
+}
+
+type unblock struct {
+	targetuser int
+	user       int
+	clock      time.Time
+}
+
 func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	for true {
+		//done := make(chan bool)
+		fmt.Println("Please enter a Command:")
+		input, _ := reader.ReadString('\n')
+		if i := strings.Index(input, "tweet"); i < 1 {
+			fmt.Printf("Found at %d , TWEET\n", i)
+		} else if i := strings.Index(input, "view"); i < 1 {
+			fmt.Printf("great, now you get to see my history")
+		} else if i := strings.Index(input, "block"); i < 1 {
+			fmt.Printf("HOW DIRE!\n")
+		} else if i := strings.Index(input, "unblock"); i < 1 {
+			fmt.Printf("HOW FRIENDLY\n")
+		} else {
+			fmt.Printf("Command not recognized\n")
+		}
+	}
 	return
 }
