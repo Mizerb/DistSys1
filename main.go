5fb69076f189e41cba7f5b4a3b6b28442dc15fdf
Ben Mizera <mizerb@rpi.edu>
2017-09-20 23:15:10 -0400

    added seperate file for structs and an interface to link them, that way I can put all the stuff into a single place, though I might change this scheme later on.

--- main.go
+++ main.go
@@ -16,23 +16,7 @@ const ( //iota is reset to 0
 	DELETE = iota // DELETE=1
 )
 
-type tweet struct {
-	message string
-	user    int
-	clock   time.Time
-}
 
-type block struct {
-	user     int
-	follower int
-	clock    time.Time
-}
-
-type unblock struct {
-	targetuser int
-	user       int
-	clock      time.Time
-}
 
 func main() {
 
