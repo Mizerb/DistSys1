b074c3a7ca80358c1e73b70f4d4a498ae73fcf69
IanGross <grossi2@rpi.edu>
2017-10-15 18:35:20 -0400

    cleanup of comments

--- recieve.go
+++ recieve.go
@@ -9,53 +9,28 @@ import (
 The logic behind this functiion starts with inserting and deleting things from
 the blocks dict, but where does it go from there
 The bit about removing things from the record is important
-But I'll need to serach through events to figure out which one I need to remove it from
-which will be a pain
-
-Weird twists,
-2 block commands
-wait, shit what if a user block more than 1 person..
-welcom to slice town, population me...
 */
+
 func (n *Node) UpdateDict(events [][]tweet) {
 	for _, stuff := range events {
 		for _, record := range stuff {
 			if record.Event == INSERT {
-				//insert into dict I guess
-				//yeah,
-
-				//TODO handle user blocking mutlipul people
-				// likley with arrays
-				// for which I am sorry, but I have no choose
-				// I wonder if someone's implemented that yet
-				// would check but I don't have internet
 				n.Blocks[record.User][record.Follower] = true
 			} else if record.Event == DELETE {
-				//handle VERY differently.
-				// like I have to think with a a peice of paper
-				// Because I can't just add it if it already exists
-
 				//delete(n.Blocks[record.User], record.Follower)
 				n.Blocks[record.User][record.Follower] = false
-				//sure
 			}
 		}
 	}
 }
 
-/*
-UpdateLog put messages into log
-wrote this because I don't want double nested loop in another function
-*/
+//Put messages into log
 func (n *Node) UpdateLog(events [][]tweet) {
-	//
-	//n.logMutex.Lock()
 	for i, noderec := range events {
 		for _, record := range noderec {
 			n.Log[i] = append(n.Log[i], record)
 		}
 	}
-	//n.logMutex.Unlock()
 	n.writeLog()
 }
 
@@ -65,20 +40,10 @@ func (n *Node) receive(msg message) {
 	// one thread might change the clocks before another adds to the logs.
 	// which would be ... very bad
 
-	/*n.BlockMutex.Lock()
-	defer n.BlockMutex.Unlock()
-
-	n.LogMutex.Lock()
-	defer n.LogMutex.Unlock()
-
-	n.TimeMutex.Lock()
-	defer n.TimeMutex.Unlock()*/
-
 	n.NodeMutex.Lock()
 	defer n.NodeMutex.Unlock()
 
 	log.Println("Recieved message from ", msg.SendID)
-	//log.Println(msg)
 
 	//Figure which events are actually new
 	newEvent := make([][]tweet, len(n.Log))
@@ -91,12 +56,9 @@ func (n *Node) receive(msg message) {
 	}
 
 	//update dictonary
-	// crapppp
-
 	n.UpdateDict(newEvent)
 
 	//update the time array
-	//n.TimeMutex.Lock()
 	for k := range n.TimeArray[n.Id] {
 		n.TimeArray[n.Id][k] = maxInt(n.TimeArray[n.Id][k], msg.Ti[msg.SendID][k])
 	}
@@ -105,16 +67,13 @@ func (n *Node) receive(msg message) {
 			n.TimeArray[i][j] = maxInt(n.TimeArray[i][j], msg.Ti[i][j])
 		}
 	}
-	//n.TimeMutex.Unlock()
 
 	//update local log
-	// by this point in time, all I should need to do is add the new events to the log
-	// I should have verification at this point that they are infact, not currently in
-	// the log
+	// add new events to log to verify that they are not in the log
 	n.UpdateLog(newEvent)
 
 	n.CleanDict()
 
-	// why is this here?
+	//provide clarity to user that user input is still available
 	fmt.Printf("Please enter a Command: ")
 }
