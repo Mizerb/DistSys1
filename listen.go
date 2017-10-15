f1be433db9745c45370d9d29bf7ed2359efa6183
Ben Mizera <mizerb@rpi.edu>
2017-10-15 18:16:26 -0400

    upped the size

--- listen.go
+++ listen.go
@@ -31,7 +31,7 @@ func handleConn(conn net.Conn, serv *Node) {
 	//NOTE: The buffer is currently statically allocated
 	// If the message becomes too large, it will cause errors
 	//Future To Do: send byte size before sending the log
-	recvBuf := make([]byte, 4096*4)
+	recvBuf := make([]byte, 4096*10)
 	_, err := conn.Read(recvBuf)
 	recvBuf = bytes.Trim(recvBuf, "\x00")
 	if err != nil {
